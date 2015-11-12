630ff0dc25cb331e80741ba8fc057d3939b5b3f0
Gavin Davidson <gavaran@hotmail.com>
2015-11-12 14:10:37 +0000

    Renamed select.f95 to select_ex.f95

---
+++ language-fortran/src/select_ex.f95
@@ -0,0 +1,21 @@
+program select_ex
+
+  INTEGER :: Class
+  Class = 2
+
+SELECT CASE (Class)
+CASE (1)
+WRITE(*,*)  'Freshman'
+CASE (2)
+WRITE(*,*)  'Sophomore'
+!    CASE (3)
+!       WRITE(*,*)  'Junior'
+!    CASE (4)
+!       WRITE(*,*)  'Senior'
+!    CASE DEFAULT
+!       WRITE(*,*)  "Hmmmm, I don't know"
+
+END SELECT
+WRITE(*,*)  'Done'
+
+end program select_ex
