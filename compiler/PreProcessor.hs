c57f552b88292940a7c0372f2642acae34ef8063
Gavin Davidson <gavaran@hotmail.com>
2016-04-03 14:49:00 +0100

    Added andOperatorFix to PreProcessor.hs

--- compiler/PreProcessor.hs
+++ compiler/PreProcessor.hs
@@ -3,7 +3,7 @@ module PreProcessor where
 import Data.Char
 
 preProcess :: String -> String
-preProcess inputStr = orOperatorFix $ containsStatementFix $ caseStatementFix $ inputStr
+preProcess inputStr = andOperatorFix $ orOperatorFix $ containsStatementFix $ caseStatementFix $ inputStr
 --preProcess inputStr = (show $ ((head inputStr) == '\n')) ++ "\n" ++ inputStr
 --preProcess inputStr = continuationFix $ inputStr
 
@@ -23,3 +23,6 @@ containsStatementFix input = (caseInsensitive_strReplace "\ncontains" "\n contai
 
 orOperatorFix :: String -> String
 orOperatorFix input = caseInsensitive_strReplace ".or." " .or. " input
+
+andOperatorFix :: String -> String
+andOperatorFix input = caseInsensitive_strReplace ".and." " .and. " input
